832ccf796c3ca82e8b3cc7fb64f07ca59567dde6
Dan Stutzman <dan@danstutzman.com>
2015-12-24 12:04:11 -0700

    Got basic search working

--- backend_app.rb
+++ backend_app.rb
@@ -15,6 +15,10 @@ class BackendApp < Sinatra::Base
   end
 
   get '/results' do
+    query = params['q']
+    word_id = Word.find_by_word(query).word_id
+    line_ids = WordLine.where(word_id: word_id).map { |wl| wl.line_id }
+    @lines = Line.where('line_id IN (?)', line_ids)
     haml :results
   end
 end
--- models.rb
+++ models.rb
@@ -3,3 +3,11 @@ require 'active_record'
 class Line < ActiveRecord::Base
   self.primary_key = 'line_id'
 end
+
+class Word < ActiveRecord::Base
+  self.primary_key = 'line_id'
+end
+
+class WordLine < ActiveRecord::Base
+  self.table_name = 'words_lines'
+end
