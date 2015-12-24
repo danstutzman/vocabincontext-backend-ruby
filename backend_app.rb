bbd4143190977712566c88d8a69f588c5ba0f86c
Dan Stutzman <dan@danstutzman.com>
2015-12-24 13:39:00 -0700

    Embolden search term in results.

--- backend_app.rb
+++ backend_app.rb
@@ -17,8 +17,31 @@ class BackendApp < Sinatra::Base
   get '/results' do
     query = params['q']
     word_id = Word.find_by_word(query).word_id
-    line_ids = WordLine.where(word_id: word_id).map { |wl| wl.line_id }
-    @lines = Line.where('line_id IN (?)', line_ids)
+    line_words = LineWord.where(word_id: word_id)
+    @lines = Line.where('line_id IN (?)', line_words.map { |lw| lw.line_id })
+    line_by_line_id = {}
+    @lines.each do |line|
+      line_by_line_id[line.line_id] = line
+      line.line_words = []
+    end
+    line_words.each do |line_word|
+      line_by_line_id[line_word.line_id].line_words.push line_word
+    end
+    @lines.each do |line|
+      line.line_html = line.line
+      line.line_words.each do |line_word|
+        line.line_html[line_word.begin_index...line_word.begin_index] += '<b>'
+        line.line_words.each do |line_word2|
+          line_word2.begin_index += 3 if line_word2.begin_index > line_word.begin_index
+          line_word2.end_index += 3   if line_word2.end_index   > line_word.begin_index
+        end
+        line.line_html[line_word.end_index...line_word.end_index] += '</b>'
+        line.line_words.each do |line_word2|
+          line_word2.begin_index += 4 if line_word2.begin_index > line_word.end_index
+          line_word2.end_index += 4   if line_word2.end_index   > line_word.end_index
+        end
+      end
+    end
     haml :results
   end
 end
--- models.rb
+++ models.rb
@@ -2,12 +2,14 @@ require 'active_record'
 
 class Line < ActiveRecord::Base
   self.primary_key = 'line_id'
+
+  attr :line_words, true
+  attr :line_html, true
 end
 
 class Word < ActiveRecord::Base
-  self.primary_key = 'line_id'
+  self.primary_key = 'word_id'
 end
 
-class WordLine < ActiveRecord::Base
-  self.table_name = 'words_lines'
+class LineWord < ActiveRecord::Base
 end
