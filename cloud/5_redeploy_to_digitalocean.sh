6e63c97d850ca54ef6d3718f837ef0f789bb210e
Dan Stutzman <dan@danstutzman.com>
2015-12-30 22:07:07 -0700

    Don't rsync *.wav files.

--- cloud/5_redeploy_to_digitalocean.sh
+++ cloud/5_redeploy_to_digitalocean.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 
 INSTANCE_IP=`tugboat droplets | grep vocabincontext | egrep -oh "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"`
 echo INSTANCE_IP=$INSTANCE_IP
-rsync -e "ssh -l web" -rv .. root@$INSTANCE_IP:/var/www/vocabincontext --exclude vendor --exclude ".*"
+rsync -e "ssh -l web" -rv .. root@$INSTANCE_IP:/var/www/vocabincontext --exclude vendor --exclude ".*" --exclude "*.wav"
 
 tugboat ssh vocabincontext <<EOF
 chown -R web:web /var/www/vocabincontext
