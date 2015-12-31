f18aa1cf01e98719343e38e312073a26216e7993
Dan Stutzman <dan@danstutzman.com>
2015-12-31 15:26:52 -0700

    Need 'gem install unicode-utils'

--- cloud/7_start_gcloud_instance.sh
+++ cloud/7_start_gcloud_instance.sh
@@ -9,4 +9,4 @@ gcloud compute instances create speech1 \
   --image speech-snapshot \
   --machine-type n1-highmem-2 \
   --preemptible \
-  --metadata "startup-script=if [ ! -e /home/daniel/process.rb ]; then /usr/bin/gsutil rsync gs://speech-danstutzman /home/daniel; fi; chown daniel:daniel /home/daniel/process.rb; sudo -u daniel /usr/bin/ruby /home/daniel/process.rb $VIDEO_ID 2>&1 | tee -a /var/log/process.log"
+  --metadata "startup-script=sudo gem install unicode-utils; if [ ! -e /home/daniel/process.rb ]; then /usr/bin/gsutil rsync gs://speech-danstutzman /home/daniel; fi; chown daniel:daniel /home/daniel/process.rb; sudo -u daniel /usr/bin/ruby /home/daniel/process.rb $VIDEO_ID 2>&1 | tee -a /var/log/process.log"
