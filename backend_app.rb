a02062f479730af5adc1030ddc6bcd5ebe1407dd
Dan Stutzman <dan@danstutzman.com>
2016-01-07 16:55:26 -0700

    Add 0.5 second fade-in before excerpt.

--- backend_app.rb
+++ backend_app.rb
@@ -182,11 +182,15 @@ class BackendApp < Sinatra::Base
     begin_millis  = Integer(params[:begin_millis])
     end_millis    = Integer(params[:end_millis])
     begin_time    = begin_millis / 1000.0
-    duration_time = (end_millis - begin_millis) / 1000.0
+    begin_fade    = [begin_time - 0.5, 0].max
+    duration_time = (end_millis / 1000.0) - begin_fade
     full_path = download_wav youtube_video_id
     excerpt_path = "/tmp/youtube/excerpt-#{begin_millis}-#{end_millis}.wav"
     command = ["/usr/local/bin/sox", full_path, excerpt_path,
-      'trim', begin_time.to_s, duration_time.to_s]
+      'trim', begin_fade.to_s, duration_time.to_s,
+      'fade', 't', (begin_time - begin_fade).to_s,
+    ]
+    puts command.join(' ')
     stdout, stderr, status = Open3.capture3(*command)
     raise "Command #{command} had stderr #{stderr}" if !File.exists? excerpt_path
     data = File.read excerpt_path
