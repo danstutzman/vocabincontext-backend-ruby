82e732563ab6b48d292aa8d7288112fca886e6b6
Dan Stutzman <dan@danstutzman.com>
2016-01-07 17:05:46 -0700

    Backup some alignments

--- backup/videos.sql
+++ backup/videos.sql
@@ -5,6 +5,10 @@ create table videos (
 COPY videos FROM STDIN WITH CSV HEADER;
 youtube_video_id, song_source_num
 WxfXzywKzYs,970984
+tOCIsy9tYJs,1065635
+cMFVtJJ9NNw,1695356
+s8BQUlhPR5w,1778381
+K2aByptv2ng,1912629
 hlw9YNIPUiE,2056423
 hlw9YNIPUiE,2056423
 \.
