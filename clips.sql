adf315d60bef6732520d1fc7ce7ee2bd4f9f90d8
Dan Stutzman <dan@danstutzman.com>
2016-12-09 12:31:32 -0700

    Delete old table

--- clips.sql
+++
@@ -1,9 +0,0 @@
-create table clips (
-  clip_id serial,
-  rough_begin_syllable int,
-  rough_end_syllable int,
-  begin_ms int not null,
-  end_ms int not null,
-  begin_char int not null,
-  end_char int not null
-);
